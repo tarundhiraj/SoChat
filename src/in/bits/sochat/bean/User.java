480a584eac3f1cd282a5328f0055277ac98dc99a
deepakalternate <d3epak91@gmail.com>
2015-09-10 13:48:15 +0530

    Added flag to Message

---
+++ src/in/bits/sochat/bean/User.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package in.bits.sochat.bean;
+
+/**
+ *
+ * @author Deepak
+ */
+public class User {
+    
+    private String ip;
+    private String username;
+
+    /**
+     * @return the ip
+     */
+    public String getIp() {
+        return ip;
+    }
+
+    /**
+     * @param ip the ip to set
+     */
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    /**
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+
+    /**
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+}
