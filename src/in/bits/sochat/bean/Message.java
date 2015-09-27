461fe7ac440555438e8aa051c5ee005858a6e737
tarun <tarun@172.17.13.75>
2015-09-27 19:01:54 +0530

    Added Doc Comments

--- src/in/bits/sochat/bean/Message.java
+++ src/in/bits/sochat/bean/Message.java
@@ -4,9 +4,16 @@ import java.io.Serializable;
 import java.sql.Time;
 
 /**
- *
- * @author Tarun
- */
+     * 
+     * Message bean created to have a template for messages being exchanged between the server and clients.
+     * The properties of message have been implemented as variables.<br/>
+     * The variables are:<br/>
+     * 1. type: denotes the type of the message being exchanged.<br/>
+     * 2. user: username of the sender of the message.<br/>
+     * 3. message: contains the body of the message.<br/>
+     * 4. time: holds the timestamp of the message, depending on situation it can be time sent or time received.<br/>
+     * 5. receiver: (used mostly for unicasting) hold the username of the intended receiver of the message as specified by the sender.<br/>
+    */
 public class Message implements Serializable{
     
     private Type type;
@@ -14,8 +21,17 @@ public class Message implements Serializable{
     private String message;
     private Time time;
     private String receiver;
+    
+    /**
+     * Constructor of the bean which takes the parameters as explained above.
+     * @param type
+     * @param user
+     * @param message
+     * @param time
+     * @param receiver 
+     */
 
-    public Message(Type type, String user, String message, Time time, String receiver) {
+public Message(Type type, String user, String message, Time time, String receiver) {
         this.type = type;
         this.user = user;
         this.message = message;
@@ -23,36 +39,70 @@ public class Message implements Serializable{
         this.receiver = receiver;
     }
     
+/**
+     * Below are the getters and setters for the variables of the bean.
+     */
+    
+    /**
+     * @return the type
+     */
        
     public Type getType() {
         return type;
     }
+    
+    
+    /**
+     * @param type the type to set
+     */
+
 
     public void setType(Type type) {
         this.type = type;
     }
-
     
+     /**
+     * @return the time
+     */
+       
     
     public Time getTime() {
         return time;
     }
+    /**
+     * @param time the time to set
+     */
+
 
     public void setTime(Time time) {
         this.time = time;
     }
+    
+    /**
+     * @return the user
+     */
+    
 
     public String getUser() {
         return user;
     }
 
+    /**
+     * @param user the user to set
+     */
     public void setUser(String user) {
         this.user = user;
     }
+    /**
+     * @return the message
+     */
 
     public String getMessage() {
         return message;
     }
+    /**
+     * @param message the message to set
+     */
 
     public void setMessage(String message) {
         this.message = message;
--- src/in/bits/sochat/bean/Type.java
+++ src/in/bits/sochat/bean/Type.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package in.bits.sochat.bean;
-
 /**
- *
- * @author tarun
+ * 
+ * enum to hold the various types of messages that are being exchanged between clients and server.
+ * The types of messages being sent are:
+ * 1. CHAT: Regular messages sent for the purpose of chatting in the group.
+ * 2. UNICAST: Messages used for chatting one on one with another user.
+ * 3. REQUEST: Message sent to request another user to initiate one on one chat.
+ * 4. ACCEPT: Message sent when the other user accepts request for one on one chat.
+ * 5. REJECT: Message sent when the other user rejects the request for one on one chat.
+ * 6. DISCONNECT: Message sent when the user wants to disconnect from the chat.
+ * 7. LOGOUT: Message sent when the user wants to leave the group chat.
+ * 8. LIST: Used by server for the purpose of updating list of online users.
+ * 9. CONFLICT: Used by server to inform user of a username conflict.
  */
+
 public enum Type{
     
     CHAT("CHAT"),
@@ -26,6 +36,11 @@ public enum Type{
     }
     private String typeOfMessage;
 
+     /**
+     * 
+     * @return the type of message
+     */
+ 
 public String getTypeOfMessage(){
        return typeOfMessage;
 }
